35fb5f0f1e5c6feb551a30499f38b4f0f1bf63f1
FrankXu7 <1248456937@qq.com>
2021-07-27 12:09:33 +0800

    完成桶排序和计数排序算法实现

---
+++ codes/BucketSort.cpp
@@ -0,0 +1,79 @@
+/**************************************************************************************************
+ * 【桶排序算法】
+ *
+ * 【基本思路】
+ * 生成一个序列作为桶，序列中每个元素被当作是一个桶，以下标值作为分类，某个桶只放置其下标所表示的元素。
+ * 为了保证数组元素一定有一个对应下标数值的桶，需要找出数组的最大值，并以此作为桶序列的大小。然后再次
+ * 遍历数组，将元素放到桶序列中下标与数组元素相同某个桶内。最后，按需要的顺序遍历桶序列，将非空的桶内
+ * 元素一一取出，追加到数组，即得到一个有序数组。
+ *
+ * 【性能分析】
+ * 时间复杂度：O(N) 实际上是O(3N)，找出最大元素，将元素放入桶内，从桶中取出元素，都要进行一次遍历
+ * 空间复杂度：O(M+N) M表示桶的个数，可能会远远大于元素数量N
+ * 稳定性：稳定 稳定性取决于桶的数据结构，若用栈作为桶是不稳定的，因为可以做到稳定，所以桶排序是稳定的
+ * 
+ * 【算法总结】
+ * 从桶的生成可以看出，桶排序只适合于数据数值大小均匀分布的情况，即最值跨度不会太大时，其排序效率是较高的。
+ * 如果跨度过大，如数组 [1,2,3,1000]，虽然只有4个元素，却需要创建1000个桶结构，极大的浪费空间。虽然桶排序
+ * 理解实现都极为简单，但除非是在要求特别高（比如时间复杂度要低于O(NlogN)），或确定数组元素最值跨度会小于
+ * 等于数组长度，否则一般不会选择桶排序算法，即使如此，也更倾向于考虑计数排序算法。
+ * 桶排序，和计数排序以及基数排序一样，是一种非比较类型的排序，排序过程中并没有发生元素的比较。
+ * 
+ * @author FrankX
+ * @date 2021-07-27
+ **************************************************************************************************/
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+/** NumType为基础数字类型 */
+template<typename NumType>
+void BucketSort(vector<NumType>& dataArr)
+{
+	// 找出最大元素 
+	typename vector<NumType>::iterator itr = dataArr.begin();
+	NumType maxNum = *(itr++);
+	for (; itr != dataArr.end(); ++itr)
+	{
+		if (*itr > maxNum) maxNum = *itr;
+	}
+
+	vector<deque<NumType>> buckets;
+	// +1相当于将下标0元素弃用，便于逻辑理解 
+	buckets.resize(static_cast<unsigned int>(maxNum + 1));
+
+	// 将元素放在桶里 
+	for (itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+	{
+		buckets[*itr].emplace_back(*itr);
+	}
+
+	unsigned int idx = 0;
+	// 依次从桶里取出元素 
+	for (typename vector<deque<NumType>>::iterator itd = buckets.begin(); itd != buckets.end(); ++itd)
+	{
+		// 运算符优先级（从高到低）：间接成员运算符(->) 直接成员运算符(.) 逻辑非(!) 解除引用(*) 
+		while (!(*itd).empty())
+		{
+			dataArr[idx++] = (*itd).front();
+			(*itd).pop_front();
+		}
+	}
+}
+
+int main()
+{
+	vector<unsigned int> dataArr = { 8,4,1,6,3,5,7,2,9,7,366,5,3 };
+
+	cout << "Array data:\n";
+	for (vector<unsigned int>::iterator itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+		cout << *itr << ", ";
+	BucketSort(dataArr);
+	cout << "\nAfter Bucket Sort:\n";
+	for (vector<unsigned int>::iterator itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+		cout << *itr << ", ";
+
+	cout << endl;
+	return 0;
+}
---
+++ codes/CountingSort.cpp
@@ -0,0 +1,76 @@
+/**************************************************************************************************
+ * 【计数排序算法】
+ *
+ * 【基本思路】
+ * 生成一个序列，以生成序列中元素下标的下标值作为分类，统计在数组中，元素值等于序列下标时的数量，相较于桶排序，
+ * 这个统计行为减少了一定的空间复杂度。和桶排序一样，为了保证数组元素一定有一个对应下标值的生成序列元素，
+ * 需要找出数组的最大值。然后再次遍历数组，将生成序列中下标值与数组元素相同序列元素+1. 最后，按需要的顺序
+ * 遍历生成序列，将非零的序列元素，取出元素值对应数量的下标值，追加到数组，即得到一个有序数组。
+ *
+ * 【性能分析】
+ * 时间复杂度：O(N) 实际上是O(3N)，找出最大元素，将元素个数统计到序列内，从序列中生成元素，都要进行一次遍历
+ * 空间复杂度：O(M) M表示生成序列元素的个数，可能会远远大于元素数量N
+ * 稳定性：稳定 
+ *
+ * 【算法总结】
+ * 和桶排序一样，只适合于数组元素最值跨度不大，比如跨度小于等于数组长度的情况。虽然计数排序理解实现都极为简单，
+ * 但除非是在要求特别高（比如时间复杂度要低于O(NlogN)），或确定数组元素最值跨度会小于等于数组长度，
+ * 否则一般不会选择计数排序算法，当需要桶排序时，优先考虑计数排序。
+ * 计数排序，和桶排序以及基数排序一样，是一种非比较类型的排序，排序过程中并没有发生元素的比较。
+ *
+ * @author FrankX
+ * @date 2021-07-27
+ **************************************************************************************************/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/** NumType为基础数字类型 */
+template<typename NumType>
+void CountingSort(vector<NumType>& dataArr)
+{
+	// 找出最大值用于确定序列大小 
+	typename vector<NumType>::iterator itr = dataArr.begin();
+	NumType maxNum = *(itr++);
+	for (typename vector<NumType>::iterator itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+	{
+		if (*itr > maxNum) maxNum = *itr;
+	}
+
+	vector<unsigned int> sequence;
+	// +1相当于将下标0元素弃用，便于逻辑理解 
+	sequence.resize(static_cast<unsigned int>(maxNum + 1));
+
+	// 统计元素个数 
+	for (itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+	{
+		++sequence[*itr];
+	}
+
+	unsigned int idx = 0;
+	// 依次从序列里生成元素追加到数组 
+	for (unsigned int n = 0; n <= maxNum; ++n)
+	{
+		while (sequence[n] > 0)
+		{
+			dataArr[idx++] = n;
+			--sequence[n];
+		}
+	}
+}
+
+int main()
+{
+	vector<unsigned int> dataArr = { 8,4,1,6,3,5,7,2,9,7,366,5,3 };
+
+	cout << "Array data:\n";
+	for (vector<unsigned int>::iterator itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+		cout << *itr << ", ";
+	CountingSort(dataArr);
+	cout << "\nAfter Counting Sort:\n";
+	for (vector<unsigned int>::iterator itr = dataArr.begin(); itr != dataArr.end(); ++itr)
+		cout << *itr << ", ";
+
+	cout << endl;
+	return 0;
+}
